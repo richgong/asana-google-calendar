ab968fa5fdff59b228ce0131a29852a1d07942cc
Richard Gong <richard.gong@instacart.com>
2019-03-29 23:56:05 -0700

    functional script

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A simple Ruby script for calling Asana from the command-line.
 
-I've adapted the code from this [asana-client](https://github.com/tmacwill/asana-client).
+Inspired by this: [asana-client](https://github.com/tmacwill/asana-client) -- but dramatically simplified (and fewer features).
 
 # Why?
 
@@ -17,6 +17,43 @@ This way, I can just edit the script -- and not worry about having to publish a 
 
 # To install
 
-- Git clone this repo
-- Make an alias in your `.bash_profile` to call the script in this repo
+First, get an `api_key`:
 
+- Open up Asana web app
+- Open "My Profile Settings"
+- Open the tab "Apps"
+- Click "Manage Developer Apps"
+- Click "+ Create New Personal Access Token"
+- Copy the secret API key
+
+Then figure out what your `user_id` and `workspace_id` are:
+
+- [https://app.asana.com/api/1.0/users](https://app.asana.com/api/1.0/users)
+- [https://app.asana.com/api/1.0/workspaces](https://app.asana.com/api/1.0/workspaces)
+
+Then create a file at `~/.asana-client`, like this:
+
+	api_key: 0/111111
+	workspace_id: 1111111
+	user_id: 1111111
+
+Then, clone this repo.
+
+Then, make an alias in your `.bash_profile` to call the script in this repo. I have something like this in my `.bash_profile`:
+
+	alias asana="~/code/asana-ruby-script/asana.rb"
+
+# Usage
+
+    # show tasks assigned to me
+    asana
+    
+    # new task for me
+    asana n <task description>
+    asana n Take out the trash
+    
+    # complete a task
+    asana d <task_id>
+    
+    # list projects
+    asana p
---
+++ asana.rb
@@ -0,0 +1,93 @@
+#!/usr/bin/env ruby
+
+require "json"
+require "net/https"
+require "yaml"
+require "chronic"
+
+
+module Asana
+  def Asana.init
+    begin
+      @@config = YAML.load_file File.expand_path "~/.asana-client"
+      @user_id = @@config['user_id']
+      @workspace_id = @@config['workspace_id']
+      # puts "User: #{@user_id} Worskpace: #{@workspace_id}"
+    rescue
+      abort "Config error: ~/.asana-client.\nSee https://github.com/richgong/asana-ruby-script for instructions."
+    end
+  end
+
+  def Asana.parse(args)
+    if args.empty?
+      tasks = Asana.get "tasks?workspace=#{@workspace_id}&assignee=me&completed_since=now"
+      tasks["data"].each do |task|
+        puts "#{task['id']}) #{task['name']}"
+      end
+      exit
+    end
+
+    cmd = args.shift
+    value = args.join ' '
+
+    case cmd
+    when 'd'
+      if value =~ /^(\d+)$/
+        Asana.put "tasks/#{$1}", { "completed" => true }
+        puts "Task completed!"
+      else
+        puts "Missing task ID"
+      end
+    when 'p'
+      projects = Asana.get "projects?workspace=#{@workspace_id}&archived=false"
+      projects["data"].each do |project|
+        puts project['name']
+      end
+    when 'a'
+      Asana.post "tasks", {
+          "workspace" => @workspace_id,
+          "name" => value,
+          "assignee" => 'me',
+      }
+      # add task to project
+      # Asana.post "tasks/#{task['data']['id']}/addProject", { "project" => project.id }
+    else
+      abort "Unknown command: #{cmd}"
+    end
+  end
+
+  def Asana.get(url)
+    return Asana.http_request(Net::HTTP::Get, url, nil, nil)
+  end
+
+  def Asana.put(url, data, query = nil)
+    return Asana.http_request(Net::HTTP::Put, url, data, query)
+  end
+
+  def Asana.post(url, data, query = nil)
+    return Asana.http_request(Net::HTTP::Post, url, data, query)
+  end
+
+  def Asana.http_request(type, url, data, query)
+    uri = URI.parse "https://app.asana.com/api/1.0/#{url}"
+    http = Net::HTTP.new uri.host, uri.port
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    header = {
+        "Content-Type" => "application/json"
+    }
+    req = type.new("#{uri.path}?#{uri.query}", header)
+    req.basic_auth @@config["api_key"], ''
+    if req.respond_to?(:set_form_data) && !data.nil?
+      req.set_form_data data
+    end
+    res = http.start { |http| http.request req  }
+    return JSON.parse(res.body)
+  end
+end
+
+
+if __FILE__ == $0
+  Asana.init
+  Asana.parse ARGV
+end
